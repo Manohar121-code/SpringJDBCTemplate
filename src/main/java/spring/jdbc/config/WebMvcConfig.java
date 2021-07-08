f95a46061336c81c2d0675e283d90118fb84d363
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2021-07-08 09:51:35 +0530

    Initial commit

---
+++ src/main/java/spring/jdbc/config/WebMvcConfig.java
@@ -0,0 +1,42 @@
+package spring.jdbc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import spring.jdbc.dao.StudentDao;
+import spring.jdbc.dao.StudentDaoImpl;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("spring.jdbc.*")
+public class WebMvcConfig {
+	@Bean
+	InternalResourceViewResolver viewResolver() {
+
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+
+		vr.setPrefix("/");
+		vr.setSuffix(".jsp");
+		return vr;
+	}
+
+	@Bean
+	DriverManagerDataSource getDataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/sampledb");
+		ds.setUsername("root");
+		ds.setPassword("manah");
+
+		return ds;
+	}
+
+	@Bean
+	public StudentDao getUserDao() {
+		return new StudentDaoImpl(getDataSource());
+	}
+}
---
+++ src/main/java/spring/jdbc/dao/StudentDao.java
@@ -0,0 +1,17 @@
+package spring.jdbc.dao;
+
+import java.util.List;
+
+import spring.jdbc.model.Student;
+
+public interface StudentDao {
+	public int create(Student student);
+
+	public List<Student> read();
+
+	public List<Student> findStudentById(int studentId);
+
+	public int update(Student student);
+
+	public int delete(int studentId);
+}
